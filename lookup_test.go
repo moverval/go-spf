7a2ed4ef1c961fb51826f5211c90075b613f277a
MMNN321 <mmnn321@users.noreply.github.com>
2023-03-31 12:38:59 +0200

    Add lookup tests

---
+++ lookup_test.go
@@ -0,0 +1,19 @@
+package spf_test
+
+import (
+	"testing"
+
+	"github.com/moverval/go-spf"
+)
+
+func TestLookupSPF(t *testing.T) {
+	record, err := spf.LookupSPF("gmail.com", "8.8.8.8:53")
+
+	if err != nil {
+		t.Errorf("No record found: %s", err)
+	}
+
+	if !spf.IsSPF(record) {
+		t.Errorf("Value is not spf record")
+	}
+}
