9841421a9138197a97b534386f332f3acb79ae43
MMNN321 <mmnn321@users.noreply.github.com>
2023-03-31 12:39:42 +0200

    Add interpreter tests

---
+++ interpreter_test.go
@@ -0,0 +1,36 @@
+package spf_test
+
+import (
+	"net"
+	"testing"
+
+	"github.com/moverval/go-spf"
+)
+
+func TestValidationPass(t *testing.T) {
+	// Check if google mail server can send mail from gmail.com
+	result, err := spf.ValidateIP(net.ParseIP("35.190.247.10"), "gmail.com", "8.8.8.8:53", 10)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if result != spf.PassQualifier {
+		t.Errorf("False Qualifier. Expected %q, got %q", spf.PassQualifier, result)
+	}
+}
+
+func TestValidationSoftFail(t *testing.T) {
+	// Check if local ip can send mail as gmail.com
+	result, err := spf.ValidateIP(net.ParseIP("192.168.178.50"), "gmail.com", "8.8.8.8:53", 10)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if result != spf.SoftFailQualifier {
+		t.Errorf("False Qualifier. Expected %q, got %q", spf.SoftFailQualifier, result)
+	}
+}
